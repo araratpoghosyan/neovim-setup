bd76af7762d0a02e219c2d56a8e91d54fef24604
Ararat Poghosyan <ararat.poghosyan@gmail.com>
2024-03-06 23:47:05 +0400

    Initial commit

---
+++ lua/config/options.lua
@@ -0,0 +1,17 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+
+-- Make cursor bold in all modes
+vim.opt.guicursor = ""
+
+-- Configure tabs
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+-- Disable unnecessary providers
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_ruby_provider = 0
+vim.g.loaded_python3_provider = 0
