0d6eba040356a493e43c531039aee4355dad39d3
Ararat Poghosyan <ararat.poghosyan@gmail.com>
2024-05-31 16:11:18 +0400

    Disable copilot and add go

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -13,8 +13,9 @@ require("lazy").setup({
         -- add LazyVim and import its plugins
         { "LazyVim/LazyVim", import = "lazyvim.plugins" },
         -- import any extras modules here
-        { import = "lazyvim.plugins.extras.coding.copilot" },
+        -- { import = "lazyvim.plugins.extras.coding.copilot" },
         { import = "lazyvim.plugins.extras.formatting.prettier" },
+        { import = "lazyvim.plugins.extras.lang.go" },
         { import = "lazyvim.plugins.extras.lang.json" },
         { import = "lazyvim.plugins.extras.lang.rust" },
         { import = "lazyvim.plugins.extras.lang.typescript" },
